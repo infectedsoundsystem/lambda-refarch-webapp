ebf1090c7c7d739d8961e30da30e31aa9d7be64d
Mike Shutlar <mike.shutlar@ba.com>
2018-03-12 14:05:23 +0000

    Ensure working in correct dir

--- build_package.sh
+++ build_package.sh
@@ -3,12 +3,14 @@
 PIP3=/usr/bin/pip3.4
 cwd=$(dirname "$(readlink -f "$0")")
 
+SOURCE_DIR=$cwd/lambda-functions
 TARGET_DIR=$cwd/lambda-packages
 TEMPLATE_DIR=$cwd/templates
 
 # Remove everything in target dir
 rm -rf $TARGET_DIR/*
 
+pushd $SOURCE_DIR
 # Install dependencies for functions
 for functiondir in */ ; do
 
@@ -26,6 +28,7 @@ for functiondir in */ ; do
     popd
 
 done
+popd
 
 # Packages the Lambda functions and swagger file to S3 ready for deployment, and updates the SAM template
 aws cloudformation package --template-file templates/serverless_application.yaml --s3-bucket $LAMBDA_BUCKET --output-template-file serverless-application-output.yaml --region $AWS_REGION
