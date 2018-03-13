d6a66ca1eea983434cdb10f51a33d445b8b74561
Mike Shutlar <mike.shutlar@ba.com>
2018-03-13 15:42:20 +0000

    Ensure custom resource fires on each update

--- build_prepareswagger.sh
+++ build_prepareswagger.sh
@@ -4,8 +4,10 @@
 replacements=(
     "\"s#<<region>>#$AWS_REGION#g\""
     "\"s#<<accountId>>#$AWS_ACCOUNT_ID#g\""
+    "\"s#<<timestamp>>#$(date +%s)#g\""
 )
 
 for exp in "${replacements[@]}"; do
     eval "sed -i -r -e $exp templates/swagger.yaml"
+    eval "sed -i -r -e $exp templates/serverless_application.yaml"
 done;
